ac72e3023009ab8002df37f7c9c7e328adb1d2ec
Ne0sky <99585944+Ne0sky@users.noreply.github.com>
2023-11-17 22:54:24 +0530

    added admin authorizations

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,53 +4,8 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// REGISTER
-export const register_staff = async (req, res) => {
-  try {
-    const { first_name, last_name, email, password, phone } = req.body;
-    const salt = await bcrypt.genSalt(10);
-    const passHash = await bcrypt.hash(password.toString(), salt);
-    const createdStaff = await prisma.staff.create({
-      data: {
-        first_name: first_name,
-        last_name: last_name,
-        email: email,
-        phone: phone,
-        password: passHash,
-      },
-    });
-    const staffData = { ...createdStaff, password: undefined };
-    return res.status(201).json({ message: "Staff added.", data: staffData });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
-};
 
-export const register_doctor = async (req, res) => {
-  try {
-    const { first_name, last_name, email, phone, password, dept_id, shift } =
-      req.body;
-    const salt = await bcrypt.genSalt(10);
-    const passHash = await bcrypt.hash(password, salt);
-    const createdDoctor = await prisma.doctor.create({
-      data: {
-        first_name: first_name,
-        last_name: last_name,
-        email: email,
-        phone: phone,
-        password: passHash,
-        dept_id: dept_id,
-        shift: new Date(shift)
-      },
-    });
-    const doctorData = { ...createdDoctor, password: undefined };
-    res.status(201).json({ message: "Doctor added.", data: doctorData });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
-};
+
 
 export const register_patient = async (req, res) => {
   try {
